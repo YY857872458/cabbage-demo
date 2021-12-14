a0cea2dd6c93e110ab43dd2d094b5bd8ed92ca94
Yang Yang2e <yang.yang2@thoughtworks.comm>
2021-12-14 15:36:39 +0800

    t.cards and t.getContext function

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,25 +1,25 @@
 console.log("Hello,World!");
 
 window.TrelloPowerUp.initialize({
-    "card-buttons": function (t, opts) {
-        return t.card("all").then(function (card) {
-            console.log("打印t.card ========================")
-            console.log(JSON.stringify(card, null, 2));
-        });
-    },
-    "board-buttons-cards": function (t, opts) {
+    // "card-buttons": function (t, opts) {
+    //     return t.card("all").then(function (card) {
+    //         console.log("打印t.card ========================")
+    //         console.log(JSON.stringify(card, null, 2));
+    //     });
+    // },
+    "board-buttons": function (t, opts) {
         return t.cards("all").then(function (cards) {
             console.log("打印t.cards ========================")
             console.log(JSON.stringify(cards, null, 2));
         });
     },
-    "board-buttons": function (t, opts) {
-        return t.board("all").then(function (board) {
-            console.log("打印t.board =========================")
-            console.log(JSON.stringify(board, null, 2));
-        });
-    },
-    'card-buttons-context': function (t, opts) {
+    // "board-buttons": function (t, opts) {
+    //     return t.board("all").then(function (board) {
+    //         console.log("打印t.board =========================")
+    //         console.log(JSON.stringify(board, null, 2));
+    //     });
+    // },
+    'card-buttons': function (t, opts) {
         var context = t.getContext();
         console.log("同步获取t的当前上下文t.getContext =========================")
         console.log(JSON.stringify(context, null, 2));
