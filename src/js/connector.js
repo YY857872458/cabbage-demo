c8eba1a5c3bfd0373dbf140cfe3cd82551e34646
Yang Yang2e <yang.yang2@thoughtworks.comm>
2021-12-14 14:38:44 +0800

    add t.card function

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,7 +1,18 @@
 console.log("Hello,World!");
 
 window.TrelloPowerUp.initialize({
-    "card-badges":function (t,opts){
-        return [];
-    }
-})
+    "card-buttons": function (t, opts) {
+        return t.card("all").then(function (card) {
+            console.log(JSON.stringify(card, null, 2));
+            console.log("======================")
+        });
+    },
+});
+
+window.TrelloPowerUp.initialize({
+    "board-buttons": function (t, opts) {
+        return t.cards("all").then(function (cards) {
+            console.log(JSON.stringify(cards, null, 2));
+        });
+    },
+});
