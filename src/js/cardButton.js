96b53e18dc237dad7cb93036f5260e5218788326
Yang Yang2e <yang.yang2@thoughtworks.comm>
2021-12-16 03:38:45 +0800

    put t inside the function

--- src/js/cardButton.js
+++ src/js/cardButton.js
@@ -1,18 +1,21 @@
-// const t = window.TrelloPowerUp.iframe();
 const Promise = window.TrelloPowerUp.Promise;
 
-
-onRecordBtnClick = function (t) {
-    const savedData = t.get('card', 'shared', 'key', 0);
-    if(!savedData){
-        return null;
-    }
-    t.set('card','shared',{
-        key:savedData.key+1
+onRecordBtnClick = function () {
+    const t = window.TrelloPowerUp.iframe();
+    return Promise.all(t.get('card','shared','key',0)).then(function (savedData) {
+        t.set('card','shared',{
+            key:savedData.key+1
+        })
+        console.log("savedData Key="+savedData.key)
+        return savedData.key;
     })
-    console.log('savedData'+savedData.key);
-    return savedData.key;
-    // if (isExist) {
+    // const savedData = t.get('card', 'shared', 'key', 0);
+    // if(!savedData){
+    //     return null;
+    // }
+    // console.log('savedData'+savedData.key);
+    // return savedData.key;
+    // // if (isExist) {
     //     t.set('card', 'shared', {
     //         times: isExist.times + 1
     //     })
