badf72787cd7ed89ff99c42b5f22e3680dce776e
Yang Yang2e <yang.yang2@thoughtworks.comm>
2021-12-15 11:26:03 +0800

    try card(feild) and t.get

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,29 +1,38 @@
 console.log("Hello,World!");
 
-window.TrelloPowerUp.initialize({
-    "card-buttons": function (t, opts) {
-        return t.card("idList").then(function (card) {
-            console.log("打印t.idList ========================")
-            console.log("t.get ========="+t.get('card','shared'))
-            console.log(JSON.stringify(card, null, 2));
-        });
-    }
-    // "board-buttons": function (t, opts) {
-    //     return t.cards("all").then(function (cards) {
-    //         console.log("打印t.cards ========================")
-    //         console.log(JSON.stringify(cards, null, 2));
-    //     });
-    // },
-    // "board-buttons": function (t, opts) {
-    //     return t.board("all").then(function (board) {
-    //         console.log("打印t.board =========================")
-    //         console.log(JSON.stringify(board, null, 2));
-    //     });
-    // },
-    // 'card-buttons': function (t, opts) {
-    //     var context = t.getContext();
-    //     console.log("同步获取t的当前上下文t.getContext =========================")
-    //     console.log(JSON.stringify(context, null, 2));
-    //     return [];
+const promise = window.TrelloPowerUp.Promise;
+
+// window.TrelloPowerUp.initialize({
+//     "card-buttons": function (t, opts) {
+//         return t.card("idList").then(function (card) {
+//             console.log("打印t.idList ========================")
+//             console.log(JSON.stringify(card, null, 2));
+//             console.log("t.get ========="+t.get('card','shared'))
+//         });
+//     }
+
+const fetchData = function (t) {
+    return promise.all([t.get('card', 'shared')]).spread(function (pluginData) {
+        console.log('pluginData===='+pluginData);
+    })
+}
+
+
+// "board-buttons": function (t, opts) {
+//     return t.cards("all").then(function (cards) {
+//         console.log("打印t.cards ========================")
+//         console.log(JSON.stringify(cards, null, 2));
+//     });
+// },
+// "board-buttons": function (t, opts) {
+//     return t.board("all").then(function (board) {
+//         console.log("打印t.board =========================")
+//         console.log(JSON.stringify(board, null, 2));
+//     });
+// },
+// 'card-buttons': function (t, opts) {
+//     var context = t.getContext();
+//     console.log("同步获取t的当前上下文t.getContext =========================")
+//     console.log(JSON.stringify(context, null, 2));
+//     return [];
 // }
-});
