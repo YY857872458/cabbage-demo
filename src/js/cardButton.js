ff42a5eb026932410f32e5c659c66972e63ce886
Yang Yang2e <yang.yang2@thoughtworks.comm>
2021-12-16 02:54:05 +0800

    add t as reference

--- src/js/cardButton.js
+++ src/js/cardButton.js
@@ -2,19 +2,19 @@ const t = window.TrelloPowerUp.iframe();
 const Promise = window.TrelloPowerUp.Promise;
 
 
-onRecordBtnClick = function () {
-    Promise.get(t.get('card', 'shared')).then(function (changeTime) {
+onRecordBtnClick = function (t) {
+    return Promise.get(t.get('card', 'shared')).then(function (changeTime) {
         if (changeTime.times && changeTime.times > 0) {
             t.set('card', 'shared', {
                 times: changeTime.times + 1
             })
-            console.log('true'+changeTime.times)
+            console.log('true' + changeTime.times)
             return changeTime.times;
         } else {
             t.set('card', 'shared', {
                 times: 1
             })
-            console.log('false'+changeTime.times)
+            console.log('false' + changeTime.times)
         }
     })
 }
