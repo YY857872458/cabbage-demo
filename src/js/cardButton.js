967e8eb266a30ac51caff32147c32fdc3784e56e
Yang Yang2e <yang.yang2@thoughtworks.comm>
2021-12-16 03:12:40 +0800

    remove promise return

--- src/js/cardButton.js
+++ src/js/cardButton.js
@@ -3,22 +3,34 @@ const Promise = window.TrelloPowerUp.Promise;
 
 
 onRecordBtnClick = function (t) {
-    console.log('t.get='+t.get('card', 'shared'));
-    return Promise.all(t.get('card', 'shared')).then(function (savedData) {
-        if ( savedData && savedData.times && savedData.times > 0) {
-            t.set('card', 'shared', {
-                times: savedData.times + 1
-            })
-            console.log('true' + savedData.times)
-            return savedData.times;
-        } else {
-            t.set('card', 'shared', {
-                times: 1
-            })
-            console.log('false' + savedData.times)
-            return null;
-        }
-    })
+    const isExist = t.get('card', 'shared');
+    if (isExist) {
+        t.set('card', 'shared', {
+            times: isExist.times + 1
+        })
+        console.log('true' + isExist.times)
+        return isExist.times;
+    } else {
+        t.set('card', 'shared', {
+            times: 1
+        })
+        console.log('false' + isExist.times)
+    }
+    // return Promise.all(isExist).then(function (savedData) {
+    //     if (savedData && savedData.times && savedData.times > 0) {
+    //         t.set('card', 'shared', {
+    //             times: savedData.times + 1
+    //         })
+    //         console.log('true' + savedData.times)
+    //         return savedData.times;
+    //     } else {
+    //         t.set('card', 'shared', {
+    //             times: 1
+    //         })
+    //         console.log('false' + savedData.times)
+    //         return null;
+    //     }
+    // })
 }
 
 onSaveBtnClick = function () {
