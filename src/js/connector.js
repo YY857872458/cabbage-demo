c95ae2f1cfbad248ae5dbc5f8e3f3df9453a9789
Yang Yang2e <yang.yang2@thoughtworks.comm>
2021-12-20 15:48:01 +0800

    try t.lists

--- src/js/connector.js
+++ src/js/connector.js
@@ -12,22 +12,24 @@ const onBtnClick = function (t, opts) {
 const cardButtons = function (t, opts) {
     let currentCardVersion;
     var context = t.getContext();
-    //点开一张卡，拿到一下信息
-    t.card('id', 'name', 'desc', 'members').then(function (result) {
-        console.log('t.card: ' + JSON.stringify(result, null, 2));
-    })
-    //首先查看一下之前是否有保存的版本desc
-    t.get(context.card, 'shared', 'version', 0).then(function (lastVersion) {
-        currentCardVersion = lastVersion;
-        console.log('t.getVersion: before set->' + currentCardVersion);
-    })
-    //再存储改过需求之后版本的desc
-    t.set(context.card, 'shared', {
-        id: context.card,
-        name: t.card('name').get('name'),
-        desc: t.card('desc').get('desc'),
-        version: currentCardVersion + 1
-    })
+
+    // //点开一张卡，拿到一下信息
+    // t.card('id', 'name', 'desc', 'members').then(function (result) {
+    //     console.log('t.card: ' + JSON.stringify(result, null, 2));
+    // })
+    // //首先查看一下之前是否有保存的版本desc
+    // t.get(context.card, 'shared', 'version', 0).then(function (lastVersion) {
+    //     currentCardVersion = lastVersion;
+    //     console.log('t.getVersion: before set->' + currentCardVersion);
+    // })
+    // //再存储改过需求之后版本的desc
+    // if(t.card('desc').get('desc'))
+    // t.set(context.card, 'shared', {
+    //     id: context.card,
+    //     name: t.card('name').get('name'),
+    //     desc: t.card('desc').get('desc'),
+    //     version: currentCardVersion + 1
+    // })
 
     return [{
         text: 'Demand Changes',
@@ -36,6 +38,13 @@ const cardButtons = function (t, opts) {
         condition: 'always'
     }];
 }
+var t = window.TrelloPowerUp.iframe();
+t.list('id','name').then(function (result) {
+    console.log('t.list: '+JSON.stringify(result))
+})
+t.lists('id','name').then(function (result) {
+    console.log('t.lists: '+JSON.stringify(result))
+})
 
 window.TrelloPowerUp.initialize(
     {
