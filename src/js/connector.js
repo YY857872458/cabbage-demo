b991a843812bdabc1308db43112f5b7a091e7d45
Yang Yang2e <yang.yang2@thoughtworks.comm>
2021-12-21 16:39:55 +0800

    try list restriction

--- src/js/connector.js
+++ src/js/connector.js
@@ -28,8 +28,8 @@ const cardButtons = function (t, opts) {
         console.log('indevlistid', inDevListId);
         console.log('idlist === const :',( context.list === inDevListId));
 
-        console.log('t.get() === undefined :', !t.get(context.card, 'shared', 'originalDesc'));
-        if (context.idList === inDevListId) {
+        console.log('t.get() === undefined :', t.get(context.card, 'shared', 'originalDesc'));
+        if (context.list === inDevListId) {
             console.log('go in if statement');
             t.set(context.card, 'shared', {
                 originalDesc: t.card('desc').get('desc'),
