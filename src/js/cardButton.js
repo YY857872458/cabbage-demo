f2f7d62301931acbeea22ada2c3cf2b3e69f63c8
Yang Yang2e <yang.yang2@thoughtworks.comm>
2021-12-22 11:09:23 +0800

    lastDesc.fulfillmentValue

--- src/js/cardButton.js
+++ src/js/cardButton.js
@@ -1,4 +1,3 @@
-
 const t = window.TrelloPowerUp.iframe();
 
 const context = t.getContext();
@@ -22,15 +21,15 @@ onSaveBtnClick = () => {
     });
     t.get(context.card, 'shared', 'originalDesc').then(function (lastDesc) {
         console.log('previous saved originalDesc: ', lastDesc);
-        console.log('lastDesc.fulfillmentValue: ',lastDesc.fulfillmentValue);
-        console.log('lastDesc.fulfillmentValue typeof: ',typeof lastDesc.fulfillmentValue);
-        console.log('string diff: ',jsDiff.diffChars("a","b"));
-        console.log('lastDesc.toString: ',lastDesc.toString());
-        console.log('currentDesc.toString: ',currentDesc.toString());
-        console.log('lastDesc type: ',typeof lastDesc);
-        console.log('currentDesc type: ',typeof currentDesc);
-        const diff = jsDiff.diffChars(lastDesc.fulfillmentValue.toString(),currentDesc.toString());
-        console.log(JSON.stringify('diff: ',diff));
+        // console.log('lastDesc.fulfillmentValue: ', lastDesc.fulfillmentValue);
+        // console.log('lastDesc.fulfillmentValue typeof: ', typeof lastDesc.fulfillmentValue);
+        // console.log('string diff: ', jsDiff.diffChars("a", "b"));
+        // console.log('lastDesc.toString: ', lastDesc.toString());
+        // console.log('currentDesc.toString: ', currentDesc.toString());
+        // console.log('lastDesc type: ', typeof lastDesc);
+        // console.log('currentDesc type: ', typeof currentDesc);
+        const diff = jsDiff.diffChars(lastDesc.fulfillmentValue, currentDesc);
+        console.log(JSON.stringify('diff: ', diff));
         diff.forEach((part) => {
             // green for additions, red for deletions
             // grey for common parts
@@ -44,9 +43,9 @@ onSaveBtnClick = () => {
                 changedDesc: currentDesc,
             }).then(function () {
                 t.get(context.card, 'shared', 'changedDesc')
-                    .then(res => console.log('afterChanged desc: \n',res));
+                    .then(res => console.log('afterChanged desc: \n', res));
                 t.get(context.card, 'shared', 'desc')
-                    .then(res => console.log('previous desc: \n',res))
+                    .then(res => console.log('previous desc: \n', res))
             })
         }
     })
