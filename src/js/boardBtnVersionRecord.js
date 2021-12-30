0fe193e810b00e0be535d7421cfc04e7a0c4c42f
Yang Yang2e <yang.yang2@thoughtworks.comm>
2021-12-30 14:25:30 +0800

    try pop up version record

---
+++ src/js/boardBtnVersionRecord.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+const t = window.TrelloPowerUp.iframe();
+
+window.onVersionBoardBtnCLick = function onVersionBoardBtnCLick(text,cardId) {
+    axios.get(`http://localhost:8086/description/${cardId}`).then(list => {
+        const versionNum = parseInt(text.substring(1));
+        const lastVersionNum = versionNum - 1;
+        const lastVersionText = `v${lastVersionNum}.0`;
+        let currentData;
+        let oldData;
+        list.data.forEach(item => {
+            if (item.version === text) {
+                currentData = item;
+            }
+            if (item.version === lastVersionText) {
+                oldData = item;
+            }
+        });
+
+        const diff = Diff.diffChars(oldData.descriptions, currentData.descriptions);
+        let savedTime = currentData.createdTime;
+        console.log("currentData.createTime: -> ", savedTime);
+        console.log("typeof currentData.createTime: -> ", typeof savedTime);
+
+        return t.modal({
+            url: './versionComparisons.html',
+            args: {
+                text: diff,
+                savedTime: savedTime
+            },
+            height: 500,
+            fullscreen: false,
+            title: 'Description Comparison'
+        })
+    });
+}
